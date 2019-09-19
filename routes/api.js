2fad48f86b40b7ba575536f72fd0c0ccbf35a585
dhanushkamath <dhanush2897@gmail.com>
2019-09-19 11:46:25 +0530

    Initial Commit

---
+++ routes/api.js
@@ -0,0 +1,141 @@
+const express = require ('express')
+const router = express.Router()
+const User = require('../models/user')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const db = "mongodb+srv://mongoadmin:mongo1234@cluster0-w4udq.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(db, err => {
+    if(err) {
+        console.error('Error! ' + err)
+    }
+    else {
+        console.log("Successfully connected to MongoDB")
+    }
+})
+
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    
+    if(token === 'null'){
+        return res.status(401).send('Unauthorized request')
+    }
+
+    let payload = jwt.verify(token, 'BentleyContinental')
+    
+    if(!payload) {
+        return res.status(401).send('Unauthorized request')
+    }
+
+    req.userId = payload.subject
+    console.log('User ID : ',payload,' logged in')
+    next()
+}
+
+router.get('/', (req, res) => {
+    res.send('From API route')
+})
+
+router.post('/register', (req,res)=> {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if(error){
+            console.log(error)
+        }
+        else{
+            let payload = { subject: registeredUser._id}
+            let token = jwt.sign(payload, 'BentleyContinental')
+            res.status(200).send({token})
+        }
+    })
+})
+
+router.post('/login', (req,res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if(!user){
+                res.status(401).send("Login Failed")
+            }
+            else
+            if( user.password !== userData.password) {
+                res.status(401).send('Invalid password')
+            } else {
+                let payload = {subject: user._id}
+                let token = jwt.sign(payload, 'BentleyContinental')
+                res.status(200).send({token})
+            }
+        }
+    })
+})
+
+router.get('/events', (req,res) => {
+    let events = [
+        {
+            "name" : "John",
+            "price" : "$450",
+            "date" : "2012-04-23T18:25:43.511Z",
+            "type" : "Credit",
+            "reference" : "Gift",
+            "_id" : "1"
+
+        },
+        {
+            "name" : "Alice",
+            "price" : "$300",
+            "date" : "2012-05-23T20:00:00.511Z",
+            "type" : "Credit",
+            "reference" : "Rent",
+            "_id" : "2"
+
+        }
+
+
+    ]
+
+    res.json(events)
+})
+
+router.get('/special',verifyToken, (req,res) => {
+    let events = [
+        {
+            "name" : "John",
+            "price" : "$450",
+            "date" : "2012-04-23T18:25:43.511Z",
+            "type" : "Credit",
+            "reference" : "Gift",
+            "_id" : "1"
+
+        },
+        {
+            "name" : "Alice",
+            "price" : "$300",
+            "date" : "2012-05-23T20:00:00.511Z",
+            "type" : "Credit",
+            "reference" : "Rent",
+            "_id" : "2"
+
+        },
+        {
+            "name" : "Bob",
+            "price" : "$250",
+            "date" : "2012-06-23T19:45:19.511Z",
+            "type" : "Credit",
+            "reference" : "Groceries",
+            "_id" : "3"
+
+        },
+    ]
+
+    res.json(events)
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const PORT = 3000
+const api = require('./routes/api')
+const app = express()
+
+// Since frontend and backend are running on two different ports
+// cors middleware has to be added
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.use('/api',api)
+
+app.get('/', (req,res) => {
+    res.send('Hello from Server')
+})
+
+app.listen(PORT, ()=> {
+    console.log('Server running on localhost: ' + PORT)
+})
