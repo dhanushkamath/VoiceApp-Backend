cd0d96213a8ce97523f27f34ac60503205c35cb6
dhanushkamath <dhanush2897@gmail.com>
2019-09-20 16:06:50 +0530

    locator api with proxy implemented

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,16 +1,33 @@
 const express = require ('express')
 const router = express.Router()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const request = require('request-promise')
+
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get('/checkin', (req,res) => {
-    let lati = req.lati;
-    let longi = req.longi;
+router.get('/locator', (req,res) => {
+    let latitude = req.query.lati;
+    let longitude = req.query.longi;
+    let radius = req.query.radi;
 
-    console.log(req.query.lati)
-    console.log(req.query.longi)
+    let options = {
+        method: 'GET',
+        uri: `https://api.hsbc.com/x-open-banking/v2.2/branches/geo-location/lat/${latitude}/long/${longitude}?radius=${radius}`,
+        json: true
+      }
+
+    console.log("Locator request made with params : ",[latitude,longitude,radius])
+    console.log(console.log(options.uri))
+    
+    request(options).then(function (response) {
+        res.json(response)
+        console.log(response["data"][0]["Brand"][0]["Branch"][0]["Name"])
+    })
+    .catch(function (err) {
+        console.log(err)
+    })
 })
 
 module.exports = router
